ee9c3d8a6730a42971a872b9658d95dd55e1bded
Babu Mohammed Izhan <babuizhan1234@gmail.com>
2021-07-18 11:51:26 +0530

    this is the final one

--- messages.js
+++ messages.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-const url = ENV['MONGODB_URI']
+const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
